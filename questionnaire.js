7cf847731595bac95dee37154ccbc8c312d926a8
Vladimir NOVIKOV (MKAD001) <VNOVIK02@neptune.dkcorp.net>
2018-03-31 21:25:29 +0300

    Added proper form validation

--- questionnaire.js
+++ questionnaire.js
@@ -1,7 +1,9 @@
-//// Getting data from Google spreadsheet to load a list for assessment onload of webpage
+//// Getting data from Google spreadsheet to load a list for assessment on load of webpage
 
+// link to script built-in to the target spreadsheet, deployed as web app
 var googleListAPI = 'https://script.google.com/macros/s/AKfycbw5L1VsicC8eNY28DeiIhOi4jqtBrb82QsAvOgBTUFmpJgROih0/exec'
 
+// load list of people to assess and put the data into the webform dropdown list
 window.addEventListener('load', function(loadlist) {
 fetch(googleListAPI)
 .then(function(response) {
@@ -16,17 +18,66 @@ fetch(googleListAPI)
 .catch()
 });
 
-//// Submitting data to a published google app
+//// Submitting data to a published google web app
 var msgSuccess = document.querySelector('.alert-success');
 var msgError = document.querySelector('.alert-danger');
 
 var myForm = document.querySelector('#webform');
 var btnSubmit = document.querySelector('.btn-primary');
 
+
+//link to external google script deployed as web app 
 var googleResultAPI = 'https://script.google.com/macros/s/AKfycbyXjT273ZKUtHwwx8fFhWzMtJO5sWQr1Kth622qdBjpnZnZtBs/exec'
 
+
+var alerts = document.querySelector('.alert-list');
+//initial value if something wrong occured
+alerts.innerHTML = 'Please answer all the questions to submit your response'
+
+// sequence of actions on submit
 myForm.addEventListener('submit', function (ev) {
     ev.preventDefault();
+
+    //define variables for each form to check input validity when submit will be clicked
+    var formName = document.querySelector('#InputName');
+    var formEmail = document.querySelector('#InputEmail');
+    var fbaList = document.querySelector('.fba-list');
+    
+    debugger
+    // !! add here validation of input forms, if everything ok - process further:
+    if (formName.value.length === 0) {
+        console.log('field "Name" is empty');
+        alerts.innerHTML = 'Please fill in your name'
+        msgError.removeAttribute("hidden");
+        msgSuccess.setAttribute("hidden", "");
+    }
+    else if (formEmail.value.length === 0) {
+        console.log('field "Email" is empty');
+        alerts.innerHTML = 'Please fill in your Email'
+        msgError.removeAttribute("hidden");
+        msgSuccess.setAttribute("hidden", "");
+    }
+    //built-in bootstrap form functionality - no need to make additional validation of Email field
+
+    //// using function with regular expression to check validity
+    // else if (!validateEmail(formEmail.value)) {
+    //     console.log('please enter valid Email');
+    // }
+    else if (fbaList.value.length === 0) {
+        console.log('select your FBA');
+        alerts.innerHTML = 'Please select your FBA'
+        msgError.removeAttribute("hidden");
+        msgSuccess.setAttribute("hidden", "");
+    }
+    else if (!ValidateAll()) {
+        console.log('not all the questions were answered');
+        alerts.innerHTML = 'Please answer all the questions first'
+        msgError.removeAttribute("hidden");
+        msgSuccess.setAttribute("hidden", "");
+    }
+    else {
+    // when all checks were passed -continue to submit the form data to the DB
+    
     // somy jquery magic to convert form data into string for json
     var dataJSON = $('#webform').serialize();
     // fetch'ing GET request
@@ -35,5 +86,51 @@ myForm.addEventListener('submit', function (ev) {
     msgError.setAttribute("hidden", "");
     btnSubmit.setAttribute("hidden", "");
     msgSuccess.removeAttribute("hidden");
+
+    }
 });
 
+//function to check if all the questions were answered
+function ValidateAll() {
+    
+    var radioGroupArray = ['question1', 'question2', 'question3', 'question4', 'question5'];
+    var valid = false;
+
+    for (var j = 0; j < radioGroupArray.length; j++) {
+
+        var radios = document.getElementsByName(radioGroupArray[j]);
+        
+        if(ValidateGroup(radios)) {
+            valid = true;
+        }
+        else {
+            valid = false;
+            break;
+        }
+
+    };
+    return valid;
+};
+
+//function to check if one of question's radio button was selected
+function ValidateGroup(radios) {
+    
+    var validGroup = false;
+
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            console.log("Selected Value = " + radios[i].value);
+            validGroup = true;
+            //return true; // checked
+        }
+    };
+    return validGroup;
+};
+
+// already built-in in the Email form - no need:
+
+// regular expression to check Email address validity
+// function validateEmail(email) {
+//     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(email);
+// };
